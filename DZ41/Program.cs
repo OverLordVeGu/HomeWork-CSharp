f2d29825f7e1f089d0e155bde9e660a91b54b3a3
VeGu <VeGu@bk.ru>
2023-08-30 20:26:46 +0700

    Колличество положительных чисел из М чисел введенных пользователем

---
+++ DZ41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] FillArray(int M)   // Метод инициализирующий ввод чисел и преобразовывающий их в одномерный массив
+{
+    int[] array = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write("Enter number = ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // Метод выводящий массив на печать
+{
+    Console.WriteLine(string.Join("  ", array));
+}
+
+int CountPossitive (int [] array) // Метод подсчитывающий колличество положительных чисел в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел = "); 
+int M = Convert.ToInt32(Console.ReadLine());  // Переменная ограничевающая колличество введенных чисел
+int[] array = FillArray(M);
+PrintArray(array);
+int countPossitive = CountPossitive(array);
+Console.WriteLine($"Колличество положительных чисел = {countPossitive}");
