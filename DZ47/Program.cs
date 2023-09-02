af29a8882152453f86e2ab6e51dec617e3ba106f
VeGu <VeGu@bk.ru>
2023-09-02 21:15:11 +0700

    Двумерный массив с вещественными числами

---
+++ DZ47/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear ();
+
+int SetNumber (string message)
+{
+    Console.Write (message);
+    int number = Convert.ToInt32 (Console.ReadLine ());
+    return number;
+}
+
+double[,] FillMatrix (int rows, int columns, int minValue, int maxValue)
+{
+    double [,] matrix = new double [rows, columns];
+    var random = new Random ();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix [i,j] = Math.Round ((random.Next (minValue, maxValue) + random.NextDouble ()),2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (double [,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength (0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength (1); j++)
+        {
+            Console.Write ($"{matrix [i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = SetNumber ("Enter rows = ");
+int columns = SetNumber ("Enter columns = ");
+int minValue = SetNumber ("Enter minial value of random numbers = ");
+int maxValue = SetNumber ("Enter maximal value of random numbers = ");
+double [,] matrix = FillMatrix (rows, columns, minValue, maxValue);
+PrintMatrix (matrix);   
+
